06ef48bc08ab8294abea9340f314e699c1bf1d43
thoriqazzikraa <thoriqazzikra17@gmail.com>
2024-08-29 13:37:22 +0700

    forgot

--- dist/cjs/index.js
+++ dist/cjs/index.js
@@ -1,12 +1,15 @@
-const { getOriginalUrl, search, downloads, downloads2, downloadAlbum, downloadAlbum2, downloadTrack, downloadTrack2 } = require("./src/index.js")
+const { getOriginalUrl, search, downloads, downloads2, downloads3, downloadAlbum, downloadAlbum2, downloadAlbum3, downloadTrack, downloadTrack2, downloadTrack3 } = require("./src/index.js")
 
 module.exports = {
   getOriginalUrl,
   search,
   downloads,
   downloads2,
+  downloads3,
   downloadAlbum,
   downloadAlbum2,
+  downloadAlbum3,
   downloadTrack,
-  downloadTrack2
+  downloadTrack2,
+  downloadTrack3
 }
